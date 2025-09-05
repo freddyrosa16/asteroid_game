a8f180e0d462344bd55b3679afc3eb34a95096a6
Freddy Rosa <freddy.rosa16@gmail.com>
2025-09-04 21:52:39 -0400

    Add main game logic and constants for asteroid game

---
+++ asteroid_game/main.py
@@ -0,0 +1,16 @@
+from constants import *
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
