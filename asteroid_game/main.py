61e94681f081ecd99a4b27d8db5e324b556a070d
Freddy Rosa <freddy.rosa16@gmail.com>
2025-09-05 18:49:10 -0400

    Refactor main function to initialize player and sprite groups for better game structure

--- asteroid_game/main.py
+++ asteroid_game/main.py
@@ -1,17 +1,34 @@
 from constants import *
+from player import Player
+from circleshape import CircleShape
 import pygame
 
 def main():
     pygame.init()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     dt = 0
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         pygame.Surface.fill(screen, "black")
+
+        for obj in drawable:
+            obj.draw(screen) 
+
+        
         pygame.display.flip()
         dt = pygame.time.Clock.tick(clock, 60) / 1000
 
