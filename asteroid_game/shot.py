39b4962fe7fe8d4e8d3b92e4b3097e6077fd8dc8
Freddy Rosa <freddy.rosa16@gmail.com>
2025-09-06 10:32:33 -0400

    Refactor Shot class to use SHOT_RADIUS constant and simplify constructor

--- asteroid_game/shot.py
+++ asteroid_game/shot.py
@@ -3,13 +3,11 @@ from constants import *
 import pygame
 
 class Shot(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
-        # sub-classes must override
-        pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        # sub-classes must override
-        self.position += (self.velocity * dt) 
+        self.position += self.velocity * dt
