bf32d1f6fd26770dc0ff8091ccb7bbe5f58a500c
Freddy Rosa <freddy.rosa16@gmail.com>
2025-10-04 14:17:57 -0400

    Refactor main game loop for improved structure and readability

--- game/main.py
+++ game/main.py
@@ -1,21 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    # Initialize pygame
     pygame.init() 
-    
-    # New GUI Window 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     
-    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
+            
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
