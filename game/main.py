18bc12848e89d320b90fa1cfd644cc531d3cad83
Freddy Rosa <freddy.rosa16@gmail.com>
2025-10-04 13:00:00 -0400

    Implement main game loop and initialize Pygame

---
+++ game/main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+
+def main():
+    # Initialize pygame
+    pygame.init() 
+    
+    # New GUI Window 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Game Loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
