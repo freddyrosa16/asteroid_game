1cbdf5337c6ed1504cf50dce55a228f57eb0d887
Freddy Rosa <freddy.rosa16@gmail.com>
2025-12-15 23:28:48 -0400

    Refactor main function to initialize clock and delta time for frame rate control

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
                 return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
