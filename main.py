26a992b9a4ce2ad22a68e0893d70e7739d8cb6dd
Freddy Rosa <freddy.rosa16@gmail.com>
2025-12-19 00:12:53 -0400

    Refactor main function to initialize game objects and update logic

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 import pygame
+import sys
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    
+
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         pygame.Surface.fill(screen, "black")
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit(1)
+            for s in shots:
+                if s.collides_with(obj):
+                    log_event("asteroid_shot")
+                    s.kill()
+                    obj.split()
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
